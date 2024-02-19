fc20d8fe7b2b707fab0ae3d0431a8a8df644b092
haizk <haizkb@gmail.com>
2024-02-20 00:39:22 +0700

    feat: Update seeder to call all

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\News;
+use App\Models\NewsCategories;
+use App\Models\NewsImages;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            NewsCategoriesSeeder::class,
+            NewsImagesSeeder::class,
+            NewsSeeder::class,
+        ]);
     }
 }
