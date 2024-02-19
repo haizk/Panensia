6d02210509e88d65322d7a292868db1d3e89340f
haizk <haizkb@gmail.com>
2024-02-19 23:33:46 +0700

    feat: Add "News" route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
