e5c546c03cc8c267f7647878781fc55e239e581a
haizk <haizkb@gmail.com>
2024-02-20 00:36:19 +0700

    feat: Add "News Images" seeder

---
+++ database/seeders/NewsImagesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\NewsImages;
+
+class NewsImagesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        NewsImages::factory()->count(50)->create();
+    }
+}
