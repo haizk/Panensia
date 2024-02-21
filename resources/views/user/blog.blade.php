2f11fb8bae75b27433c0fdbf36b4412471473cbb
Humanioraaa <muhhilmynaufal03@gmail.com>
2024-02-21 14:02:17 +0700

    import asset & layouting

---
+++ resources/views/user/blog.blade.php
@@ -0,0 +1,246 @@
+@extends('user.layout.master')
+@section('content')
+    <!-- hero-section -->
+    <section class="hero-section about gap" style="background-image: url(assets/img/background.png);">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200" data-aos-duration="300">
+                    <div class="about-text">
+                        <ul class="crumbs d-flex">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="two"><a href="index.html"><i class="fa-solid fa-right-long"></i>Blog Page</a></li>
+                        </ul>
+                        <h2>Current news about Us</h2>
+                        <p>Egestas sed tempus urna et pharetra pharetra massa. Fermentum posuere urna nec tincidunt praesent
+                            semper.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6" data-aos="flip-up" data-aos-delay="300" data-aos-duration="400">
+                    <div class="restaurants-girl-img blog food-photo-section">
+                        <img alt="man" src="https://via.placeholder.com/546x516">
+                        <a href="#" class="one"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#" class="two"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#" class="three"><i class="fa-brands fa-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- posts-section-blog -->
+    <section class="posts-section-blog gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12" data-aos="fade-up" data-aos-delay="200" data-aos-duration="300">
+                    <div class="news-posts-one full">
+                        <div class="blog-post-data-img">
+                            <img alt="man" src="https://via.placeholder.com/680x450">
+                            <div class="quickeat">
+                                <a href="#">news</a>
+                                <a href="#">quickeat</a>
+                            </div>
+                        </div>
+                        <div class="blog-post-data-img">
+                            <h3>We Have Received An Award For The Quality Of Our Work</h3>
+                            <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra.
+                                Morbi enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...
+                            </p>
+                            <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                            <ul class="data">
+                                <li>
+                                    <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                                </li>
+                                <li>
+                                    <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                                </li>
+                                <li>
+                                    <h6><i class="fa-solid fa-eye"></i>132</h6>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200"
+                    data-aos-duration="300">
+                    <div class="news-posts-one blog">
+                        <img alt="man" src="https://via.placeholder.com/443x295">
+                        <div class="quickeat">
+                            <a href="#">news</a>
+                            <a href="#">quickeat</a>
+                        </div>
+                        <h3>With Quickeat you can order food for the whole day</h3>
+                        <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra. Morbi
+                            enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...</p>
+                        <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                        <ul class="data">
+                            <li>
+                                <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-solid fa-eye"></i>132</h6>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="300"
+                    data-aos-duration="400">
+                    <div class="news-posts-one blog">
+                        <img alt="man" src="https://via.placeholder.com/443x295">
+                        <div class="quickeat">
+                            <a href="#">news</a>
+                            <a href="#">quickeat</a>
+                        </div>
+                        <h3>127+ Couriers On Our Team Big Food Trends</h3>
+                        <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra. Morbi
+                            enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...</p>
+                        <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                        <ul class="data">
+                            <li>
+                                <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-solid fa-eye"></i>132</h6>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="400"
+                    data-aos-duration="500">
+                    <div class="news-posts-one blog">
+                        <img alt="man" src="https://via.placeholder.com/443x295">
+                        <div class="quickeat">
+                            <a href="#">news</a>
+                            <a href="#">quickeat</a>
+                        </div>
+                        <h3>Why You Should Optimize Your Menu for Delivery</h3>
+                        <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra. Morbi
+                            enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...</p>
+                        <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                        <ul class="data">
+                            <li>
+                                <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-solid fa-eye"></i>132</h6>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200"
+                    data-aos-duration="300">
+                    <div class="news-posts-one blog">
+                        <img alt="man" src="https://via.placeholder.com/443x295">
+                        <div class="quickeat">
+                            <a href="#">news</a>
+                            <a href="#">quickeat</a>
+                        </div>
+                        <h3>How Restaurants Can Engage with Millennials in 2022</h3>
+                        <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra. Morbi
+                            enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...</p>
+                        <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                        <ul class="data">
+                            <li>
+                                <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-solid fa-eye"></i>132</h6>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="300"
+                    data-aos-duration="400">
+                    <div class="news-posts-one blog">
+                        <img alt="man" src="https://via.placeholder.com/443x295">
+                        <div class="quickeat">
+                            <a href="#">news</a>
+                            <a href="#">quickeat</a>
+                        </div>
+                        <h3>Why You Should Optimize Your Menu for Delivery</h3>
+                        <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra. Morbi
+                            enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...</p>
+                        <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                        <ul class="data">
+                            <li>
+                                <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-solid fa-eye"></i>132</h6>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="400"
+                    data-aos-duration="500">
+                    <div class="news-posts-one blog">
+                        <img alt="man" src="https://via.placeholder.com/443x295">
+                        <div class="quickeat">
+                            <a href="#">news</a>
+                            <a href="#">quickeat</a>
+                        </div>
+                        <h3>5 Big food Trends Coming Your Way in 2022</h3>
+                        <p>Scelerisque purus semper eget duis at. Tincidunt ornare massa eget egestas purus viverra. Morbi
+                            enim nunc faucibus a pellentesque. Lobortis elementum nibh tellus molestie nunc non...</p>
+                        <a href="#">Read More<i class="fa-solid fa-arrow-right"></i></a>
+                        <ul class="data">
+                            <li>
+                                <h6><i class="fa-solid fa-user"></i>by Quickeat</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-regular fa-calendar-days"></i>01.Jan. 2022</h6>
+                            </li>
+                            <li>
+                                <h6><i class="fa-solid fa-eye"></i>132</h6>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="button-gap" data-aos="fade-up" data-aos-delay="200" data-aos-duration="300">
+                    <a href="#" class="button button-2 non">See All<i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- subscribe-section -->
+    <section class="subscribe-section gap" style="background:#fcfcfc">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6" data-aos="flip-up" data-aos-delay="200" data-aos-duration="300">
+                    <div class="img-subscribe">
+                        <img alt="Illustration" src="https://via.placeholder.com/676x403">
+                    </div>
+                </div>
+                <div class="col-lg-5 offset-lg-1" data-aos="flip-up" data-aos-delay="300" data-aos-duration="400">
+                    <div class="get-the-menu">
+                        <h2>Get the menu of your favorite restaurants every day</h2>
+                        <form>
+                            <i class="fa-regular fa-bell"></i>
+                            <input type="text" name="email" placeholder="Enter email address">
+                            <button class="button button-2">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
