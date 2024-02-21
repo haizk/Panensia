2785dbb5b4dd856617ea99f59be34fc0e99b27b8
Humanioraaa <muhhilmynaufal03@gmail.com>
2024-02-21 14:01:46 +0700

    importing asset & layouting

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,49 @@ use App\Http\Controllers\NewsController;
 //     return view('welcome');
 // });
 
-// Route::get('/{any}', function () {
-//     return view('app');
-// })->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
+
+Route::get('/{any}', function () {
+    return view('user.index');
+})->where('any', '.*');
+
+Route::get('/{any}', function () {
+    return view('user.404');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.about');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.become-partner');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.blog');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('checkout.checkout');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('contacts.contacts');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('faq.index');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.pricing-table');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.restaurants-card');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.restaurants');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.services');
+})->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('user.single-blog');
+})->where('any', '.*');
 
 Route::get('/tes', function () { return view('index/dashboard');});
