70c00d9435e54ec4146567633d7b339a71078127
nabilland <nabillasolo2015@gmail.com>
2024-03-08 20:20:44 +0700

    add authn vue router

--- resources/js/router/auth.js
+++ resources/js/router/auth.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LoginView from '../views/auth/LoginView.vue'
+import ForgotPasswordView from '../views/auth/ForgotPasswordView.vue'
+import ResetPasswordView from '../views/auth/ResetPasswordView.vue'
 
-const authRouter = createRouter({
+const loginRouter = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
@@ -10,7 +12,19 @@ const authRouter = createRouter({
             name: 'login',
             component: LoginView
         },
+        {
+            path: '/forgotPassword',
+            name: 'forgotPassword',
+            component: ForgotPasswordView,
+        },
+        {
+            path: '/resetPasswordForm/:token',
+            name: 'resetPasswordForm',
+            component: ResetPasswordView,
+            props: true,
+        },
     ]
 })
 
-export default authRouter
+
+export default loginRouter
