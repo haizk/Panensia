12759560142507e7958c3dcd2b3aa315f8ec76d4
slsabilaAura <shalsabilaauraadiar@student.uns.ac.id>
2024-03-10 16:29:41 +0700

    warna berita

--- resources/js/views/BeritaView.vue
+++ resources/js/views/BeritaView.vue
@@ -21,13 +21,13 @@ Berikut adalah kode HTML yang telah dirapikan:
       </div>
     </section>
 
-    <section >
+    <section  style=" background-image: linear-gradient(to bottom, #FFF8B5, #FFF8B5,#B2B377);">
     <div class="twoColumns container pt-lg-23 pb-lg-20 pt-md-16 pb-md-4 pt-10 pb-4" >
       <div class="row">
         <div class="col-12 col-lg-9 order-lg-3">
           <article id="content">
             <!-- card baru  -->
-            <div class="card mb-3 mt-2" style="max-width: 780px; height: 180px;  background-color:#FBFADA;">
+            <div class="card mb-3 mt-2" style="max-width: 780px; height: 180px;  background-color:#F1F5A8;">
               <div class="row g-0">
                 <div class="col-md-4">
                   <img src="https://cdn.pixabay.com/photo/2017/07/28/13/29/spices-2548653_960_720.jpg" class="img-fluid rounded-start" alt="..."  style="width:fit-content; height: fit-content;">
@@ -37,7 +37,7 @@ Berikut adalah kode HTML yang telah dirapikan:
                     <p class="card-text"><small class="text-body-secondary">penulis</small> <small>tgl liris</small></p>
                     <RouterLink to="/detailberita"><h5 class="card-title" style="color: #5ba515;">Judul Berita</h5></RouterLink>
                     <p class="card-text">summary berita ini adalah</p>
-                    <p class="card-text"><small class="text-body-secondary">Kategory</small></p>
+                    <p class="card-text"><small class="text-body-secondary btn-primary">Kategory</small></p>
                   </div>
                 </div>
               </div>
@@ -49,10 +49,10 @@ Berikut adalah kode HTML yang telah dirapikan:
         <div class="col-12 col-lg-3 order-lg-1" >
           <aside id="sidebar" >
             <section class="widget overflow-hidden mb-md-9 mb-6 ml-2">
-              <h3 class="headingVII fwEbold text-uppercase mb-4 mt-4" >Search</h3>
+              <h3 class="headingVII fwEbold text-uppercase mb-4 mt-4 " style="color: #114232;">Search</h3>
               <form action="javascript:void(0);" class="searchForm position-relative border">
-                <fieldset style="background-color: #BFEA7C;">
-                  <input type="search" class="form-control" placeholder="Search product..." >
+                <fieldset style="background-image: url(https://cdn.pixabay.com/photo/2016/09/21/04/46/barley-field-1684052_960_720.jpg); height:100px;">
+                  <input type="search" class="form-control" placeholder="Search product..."   >
                   <button class="position-absolute"><i class="icon-search"></i></button>
                 </fieldset>
               </form>
@@ -61,7 +61,7 @@ Berikut adalah kode HTML yang telah dirapikan:
             <section class="widget overflow-hidden mb-md-5 mb-3" >
               <h3 class="headingVII fwEbold text-uppercase mb-4">CATEGORIES</h3>
               <ul class="list-unstyled archiveList mb-0">
-                <li class="mb-3 " ><a href="javascript:void(0);" class="d-block">Kegiatan Panansia</a></li>
+                <li class="mb-3 " ><a href="javascript:void(0);" class="d-block text-black">Kegiatan Panansia</a></li>
                 <li class="mb-3"><a href="javascript:void(0);" class="d-block">Resep Sehat</a></li>
                 <li class="mb-3"><a href="javascript:void(0);" class="d-block">Tips and Trick</a></li>
                 <li class="mb-3"><a href="javascript:void(0);" class="d-block">Imun dan Stamina</a></li>
