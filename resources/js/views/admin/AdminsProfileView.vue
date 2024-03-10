d596187a3385ce0b2a768b04eeb4904ce51853ab
nabilland <nabillasolo2015@gmail.com>
2024-03-10 21:51:21 +0700

    add admin profile

--- resources/js/views/admin/AdminsProfileView.vue
+++ resources/js/views/admin/AdminsProfileView.vue
@@ -1,21 +1,30 @@
 <script setup>
-import { onMounted, ref, defineProps } from 'vue';
+import { onMounted, ref } from 'vue';
 import axios from 'axios';
+import { useRouter } from 'vue-router';
 import AdminNavComp from '../../components/AdminNavComp.vue';
 import AdminFooterComp from '../../components/AdminFooterComp.vue';
 
-const props = defineProps(['id']);
 const admin = ref({});
+const router = useRouter();
 
 onMounted(async () => {
-  try {
-    const response = await axios.get(`/api/admins/${props.id}`);
-    admin.value = response.data;
-  } catch (error) {
-    console.error(error);
-    alert('Error fetching admin profile');
-  }
+    try {
+        const response = await axios.get('/api/user', {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            },
+        });
+        admin.value = response.data.user;
+    } catch (error) {
+        console.error(error);
+        alert('Error fetching admin profile');
+    }
 });
+
+const navigateToChangePassword = () => {
+    router.push({ name: 'admin.change_password', params: { id: admin.value.id } });
+};
 </script>
 
 <template>
@@ -27,6 +36,8 @@ onMounted(async () => {
       <h1>Admin Profile</h1>
       <p>Name: {{ admin.name }}</p>
       <p>Email: {{ admin.email }}</p>
+      <p>Phone: {{ admin.phone }}</p>
+      <button @click="navigateToChangePassword">Change Password</button>
     </main>
     <AdminFooterComp />
   </div>
